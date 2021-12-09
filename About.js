5cc8979366f2b8fdcbaf5d4b90978e2b960b746f
SIMAN <79779796+ajgmsr999@users.noreply.github.com>
2021-12-09 12:14:45 +0530

    text

---
+++ About.js
@@ -0,0 +1,14 @@
+
+
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component 
+            <p><b>Note: This is react app he work to lowercase letter to uppercase letter convert 
+            when i click</b> = <b>Convert to Uppercase Button</b></p>
+        </div>
+    )
+}
+
---
+++ Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+        <div>
+             <p className="text-center">
+            Copyright &copy; 2021 My-App By React 
+            </p>
+        </div>
+    )
+}
